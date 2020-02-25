377a2e6940fe86ad0a5566c14565cf6fc792db64
Christian R <christianrelling@hotmail.com>
2020-02-25 13:33:48 +0100

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import Vue from 'vue';
+import App from './App.vue';
+import VueRouter from 'vue-router';
+import GameMenu from './components/GameMenu';
+
+Vue.use(VueRouter);
+Vue.config.productionTip = false;
+
+const routes = [
+  { path: '/test', component: GameMenu }
+];
+
+const router = new VueRouter({ routes });
+
+new Vue({
+  router,
+  render: h => h(App),
+}).$mount('#app')
